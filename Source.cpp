480ae450f1488f3b2231dd3d6ae56468a27ca957
jhew7 <81891289+jhew7@users.noreply.github.com>
2021-04-25 14:27:00 +1000

    Final Final Commit - Added Git Link & Academic Integrity

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,19 @@
+/******************************************************************************
+*
+*  ACADEMIC INTEGRITY DECLARATION
+*
+*   I, Jeremy Hew declare that except where I have referenced,
+*  the work I am are submitting in this attachment is my own work. I
+*  acknowledge and agree that the assessor of this assignment may,
+*  for the purpose of authenticating this assignment, reproduce it
+*  for the purpose of detecting plagiarism. I have read and am aware
+*  of the Think Education Policy and Procedure viewable online at
+*  http://www.think.edu.au/studying-at-think/policies-and-procedures/
+*
+******************************************************************************/
+
+//// GIT HUB LINK: https://github.com/jhew7/ADS103-A3-Q2-HEW_Jeremy
+
 #include <iostream>
 #include <string>
 #include <fstream>		// File Reading/Writing
